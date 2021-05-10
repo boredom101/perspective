4d230813a05a3448b3cbef8bb95bea2b0635a2a7
boredom101 <ymnewmark@gmail.com>
2021-05-10 15:56:03 +0000

    Update gh-pages to output generated at a46809a

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const dataset = [
         "object":"London bus"
     },
     {
-        "value":33,
+        "value":133,
         "type":"length",
         "object":"longest-known blue whale"
     },
@@ -76,10 +76,23 @@ const dataset = [
     }
 ];
 
+const factor = 5;
+
 function process() {
     const value = document.getElementById("input").value;
-    result = dataset.reduce((a, b) => Math.abs(b.value - value) < Math.abs(a.value - value) ? b : a);
-    document.getElementById("output").innerText = result.type + " of " + result.object;
+    weights = dataset.map((x) =>
+        Math.min(value % x.value, (value + factor) % x.value));
+    var index = 0;
+    var lowest = weights[0];
+    for (var i = 1; i < weights.length; i++) {
+        if (weights[i] < lowest && weights[i] >= 1) {
+            lowest = weights[i];
+            index = i;
+        }
+    }
+    var result = dataset[index];
+    //result = dataset.reduce((a, b) => Math.abs(b.value - value) < Math.abs(a.value - value) ? b : a);
+    document.getElementById("output").innerText = "around x" + Math.round(value / result.value) + " " + result.type + " of " + result.object;
 }
 
 function init() {
